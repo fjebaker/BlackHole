b96509eed0fd39a1f6a6e110c7e8cc0651479e02
dustpancake <fergusbkr@gmail.com>
2021-02-18 13:30:54 +0100

    change indexing syntax of geodesic

--- src/accretiondisk.jl
+++ src/accretiondisk.jl
@@ -31,13 +31,13 @@ function transform(x, y, z, α, β)
 end
 
 function calcintersect(g, d::AccretionDisk, β, intensity::Function)
-    x, y, z = g.curve[:, begin]
+    x, y, z = g[:, begin]
     z = 0 # rounding errors in ODE solve
     x, y, z = transform(x, y, z, d.α, d.β + β)
     
     mapfoldl(
         (index) -> begin
-            x2, y2, z2 = g.curve[:, index]
+            x2, y2, z2 = g[:, index]
             z2 = 0
             radius = sqrt(x2^2 + y2^2 + z2^2)
             x2, y2, z2 = transform(x2, y2, z2, d.α, d.β + β)
@@ -52,7 +52,7 @@ function calcintersect(g, d::AccretionDisk, β, intensity::Function)
             ret
         end,
         +,
-        2:size(g.curve)[2],
+        2:size(g)[2],
         init=zeros(Float64, 3)
     )
 end
